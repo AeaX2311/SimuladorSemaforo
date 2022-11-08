88cb5ac8a1dbda8ebf3f070ce8611595ff2fee8e
AeaX2311 <alan.ssuc2311@gmail.com>
2022-11-08 12:15:13 -0600

    Disenio v1 - avances

--- Semaforo/Semaforo.Designer.cs
+++ Semaforo/Semaforo.Designer.cs
@@ -24,49 +24,23 @@ namespace Semaforo {
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.btnIniciar = new System.Windows.Forms.Button();
-            this.btnDetener = new System.Windows.Forms.Button();
-            this.btnPreventivas = new System.Windows.Forms.Button();
             this.lblnumero = new System.Windows.Forms.Label();
             this.lblSemaforo = new System.Windows.Forms.Label();
-            this.btnPausa = new System.Windows.Forms.Button();
+            this.chkPreventivas = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnIniciar = new FontAwesome.Sharp.IconButton();
+            this.btnPausar = new FontAwesome.Sharp.IconButton();
+            this.btnDetener = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnIniciar
-            // 
-            this.btnIniciar.Location = new System.Drawing.Point(95, 37);
-            this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
-            this.btnIniciar.TabIndex = 0;
-            this.btnIniciar.Text = "Iniciar";
-            this.btnIniciar.UseVisualStyleBackColor = true;
-            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
-            // 
-            // btnDetener
-            // 
-            this.btnDetener.Location = new System.Drawing.Point(95, 66);
-            this.btnDetener.Name = "btnDetener";
-            this.btnDetener.Size = new System.Drawing.Size(75, 23);
-            this.btnDetener.TabIndex = 1;
-            this.btnDetener.Text = "Detener";
-            this.btnDetener.UseVisualStyleBackColor = true;
-            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
-            // 
-            // btnPreventivas
-            // 
-            this.btnPreventivas.Enabled = false;
-            this.btnPreventivas.Location = new System.Drawing.Point(208, 36);
-            this.btnPreventivas.Name = "btnPreventivas";
-            this.btnPreventivas.Size = new System.Drawing.Size(75, 23);
-            this.btnPreventivas.TabIndex = 2;
-            this.btnPreventivas.Text = "Preventivas";
-            this.btnPreventivas.UseVisualStyleBackColor = true;
             // 
             // lblnumero
             // 
             this.lblnumero.AutoSize = true;
             this.lblnumero.Font = new System.Drawing.Font("Digital-7 Mono", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblnumero.Location = new System.Drawing.Point(391, 220);
+            this.lblnumero.Location = new System.Drawing.Point(421, 61);
             this.lblnumero.Name = "lblnumero";
             this.lblnumero.Size = new System.Drawing.Size(107, 123);
             this.lblnumero.TabIndex = 3;
@@ -75,51 +49,144 @@ namespace Semaforo {
             // lblSemaforo
             // 
             this.lblSemaforo.AutoSize = true;
-            this.lblSemaforo.Location = new System.Drawing.Point(161, 295);
+            this.lblSemaforo.Location = new System.Drawing.Point(176, 274);
             this.lblSemaforo.Name = "lblSemaforo";
             this.lblSemaforo.Size = new System.Drawing.Size(35, 13);
             this.lblSemaforo.TabIndex = 6;
             this.lblSemaforo.Text = "label1";
             // 
-            // btnPausa
+            // chkPreventivas
             // 
-            this.btnPausa.Location = new System.Drawing.Point(95, 95);
-            this.btnPausa.Name = "btnPausa";
-            this.btnPausa.Size = new System.Drawing.Size(75, 23);
-            this.btnPausa.TabIndex = 7;
-            this.btnPausa.Text = "Pausa";
-            this.btnPausa.UseVisualStyleBackColor = true;
-            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
+            this.chkPreventivas.AutoSize = true;
+            this.chkPreventivas.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.chkPreventivas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.chkPreventivas.Dock = System.Windows.Forms.DockStyle.Right;
+            this.chkPreventivas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkPreventivas.Location = new System.Drawing.Point(781, 16);
+            this.chkPreventivas.Name = "chkPreventivas";
+            this.chkPreventivas.Size = new System.Drawing.Size(125, 58);
+            this.chkPreventivas.TabIndex = 8;
+            this.chkPreventivas.Text = "Preventivas";
+            this.chkPreventivas.UseVisualStyleBackColor = false;
+            this.chkPreventivas.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox1.Controls.Add(this.btnDetener);
+            this.groupBox1.Controls.Add(this.btnPausar);
+            this.groupBox1.Controls.Add(this.btnIniciar);
+            this.groupBox1.Controls.Add(this.chkPreventivas);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(909, 77);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.IconChar = FontAwesome.Sharp.IconChar.Play;
+            this.btnIniciar.IconColor = System.Drawing.Color.Black;
+            this.btnIniciar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.Location = new System.Drawing.Point(3, 16);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(147, 58);
+            this.btnIniciar.TabIndex = 10;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click_1);
+            // 
+            // btnPausar
+            // 
+            this.btnPausar.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnPausar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPausar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPausar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPausar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPausar.IconChar = FontAwesome.Sharp.IconChar.Pause;
+            this.btnPausar.IconColor = System.Drawing.Color.Black;
+            this.btnPausar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPausar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPausar.Location = new System.Drawing.Point(150, 16);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(147, 58);
+            this.btnPausar.TabIndex = 11;
+            this.btnPausar.Text = "Pausar";
+            this.btnPausar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPausar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPausar.UseVisualStyleBackColor = false;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.BackColor = System.Drawing.Color.Salmon;
+            this.btnDetener.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetener.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDetener.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDetener.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetener.IconChar = FontAwesome.Sharp.IconChar.Stop;
+            this.btnDetener.IconColor = System.Drawing.Color.Black;
+            this.btnDetener.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDetener.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDetener.Location = new System.Drawing.Point(297, 16);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(147, 58);
+            this.btnDetener.TabIndex = 12;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDetener.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDetener.UseVisualStyleBackColor = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblnumero);
+            this.panel1.Controls.Add(this.lblSemaforo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(909, 583);
+            this.panel1.TabIndex = 10;
             // 
             // Semaforo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnPausa);
-            this.Controls.Add(this.lblSemaforo);
-            this.Controls.Add(this.lblnumero);
-            this.Controls.Add(this.btnPreventivas);
-            this.Controls.Add(this.btnDetener);
-            this.Controls.Add(this.btnIniciar);
+            this.ClientSize = new System.Drawing.Size(909, 660);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(800, 0);
             this.Name = "Semaforo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Simulador semaforo";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Semaforo_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnIniciar;
-        private System.Windows.Forms.Button btnDetener;
-        private System.Windows.Forms.Button btnPreventivas;
         private System.Windows.Forms.Label lblnumero;
         private System.Windows.Forms.Label lblSemaforo;
-        private System.Windows.Forms.Button btnPausa;
+        private System.Windows.Forms.CheckBox chkPreventivas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private FontAwesome.Sharp.IconButton btnIniciar;
+        private FontAwesome.Sharp.IconButton btnPausar;
+        private FontAwesome.Sharp.IconButton btnDetener;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Semaforo/Semaforo.cs
+++ Semaforo/Semaforo.cs
@@ -10,13 +10,12 @@ namespace Semaforo {
     public partial class Semaforo : Form {
         public Semaforo() {
             InitializeComponent();
-            semaforoFacade = new SemaforoFacade();
             dActualizarVista = new DActualizarVista(movimientoNumero);
             apagar();
             pausarPrograma = false;
+            preventivasOn = false;
         }
 
-        SemaforoFacade semaforoFacade;
         private delegate void DActualizarVista();
         private DActualizarVista dActualizarVista = null;
         private Thread hiloTrabajo = null;
@@ -26,6 +25,7 @@ namespace Semaforo {
         int contadorSegundos;
         private bool semaforoVerticalEncendido;
         private bool pausarPrograma;
+        private bool preventivasOn;
 
         private void apagar() {
             color = Color.White;
@@ -33,6 +33,7 @@ namespace Semaforo {
             if(hiloTrabajo != null) hiloTrabajo.Abort();
             pausarPrograma = false;
         }
+
         private void iniciarSimulacion() {
             ejecutarSimulador = true;
             if(!pausarPrograma) {
@@ -49,14 +50,6 @@ namespace Semaforo {
             ejecutarSimulador = false;
         }
 
-        private void btnIniciar_Click(object sender, EventArgs e) {
-            iniciarSimulacion();
-        }
-
-        private void btnDetener_Click(object sender, EventArgs e) {
-            detenerSimulacion();
-        }
-
         private void movimientoNumero() {
             lblnumero.Text = numero.ToString();
             lblnumero.ForeColor = color;
@@ -64,80 +57,88 @@ namespace Semaforo {
         }
 
         private void simular() {
-            Color colorAux = Color.Green;
             int sleep = 0;
             bool banderaAux = true; //Indica el color/acciones para el proceso amarillo "parpadear"
+            bool banderaPreventivas = true; //Indica el color para el proceso preventivas "parpadear"
 
             ////Se estara ejecutando indeterminadamente hasta que se detenga manualmente el proceso
             while(ejecutarSimulador) {
-                Stopwatch timeMeasure = new Stopwatch();
-                timeMeasure.Start();
-
-                if(contadorSegundos == 27) { //Cambiar de semaforo
-                    contadorSegundos = 1; //Resetear conteo
-                    numero = 1; //Primer valor
-                    colorAux = Color.Green; //Siempre inicia en verde
-                    sleep = 500;
-                    semaforoVerticalEncendido = !semaforoVerticalEncendido; //Invertir semaforos encendidos
-                } else if(contadorSegundos < 17) { //Fijo | 15 segundos | Verde
-                    if(contadorSegundos == 16) {
-                        colorAux = Color.White; //Se "apaga". Sleep 500.
-                        numero--; //Quiero que mantenga el mismo numero
-                        banderaAux = true;
+                //Stopwatch timeMeasure = new Stopwatch();
+                //timeMeasure.Start();
+                if(!preventivasOn) {
+                    if(contadorSegundos == 27) { //Cambiar de semaforo
+                        contadorSegundos = 1; //Resetear conteo
+                        numero = 1; //Primer valor
+                        color = Color.Green; //Siempre inicia en verde
                         sleep = 500;
-                    } else {
-                        colorAux = Color.Green; //Encendido default
-                        sleep = 1000;
-                    }
-
-                    numero++;
-                } else if(contadorSegundos < 21) { //Parp | 03 segundos | Verde
-                    numero = contadorSegundos - 16; //Vuelve a ir del 1-3. Sleep 500.
-                    sleep = 500;
-                    if(banderaAux) {
-                        if(contadorSegundos == 20) {
-                            colorAux = Color.Yellow;
-                            numero = 1;
+                        semaforoVerticalEncendido = !semaforoVerticalEncendido; //Invertir semaforos encendidos
+                    } else if(contadorSegundos < 17) { //Fijo | 15 segundos | Verde
+                        if(contadorSegundos == 16) {
+                            color = Color.White; //Se "apaga". Sleep 500.
+                            numero--; //Quiero que mantenga el mismo numero
+                            banderaAux = true;
                             sleep = 500;
-                        }else {
-                            colorAux = Color.Green;
+                        } else {
+                            color = Color.Green; //Encendido default
+                            sleep = 1000;
                         }
-                    } else {
-                        colorAux = Color.White;
-                        numero--;
-                        contadorSegundos--;
-                    }
 
-                    banderaAux = !banderaAux;
-                } else if(contadorSegundos < 24) { //Fijo | 03 segundos | Amarillo
-                    numero = contadorSegundos - 19; //Vuelve a ir del 1-3.
-                    colorAux = Color.Yellow;
+                        numero++;
+                    } else if(contadorSegundos < 21) { //Parp | 03 segundos | Verde
+                        numero = contadorSegundos - 16; //Vuelve a ir del 1-3. Sleep 500.
+                        sleep = 500;
+
+                        if(banderaAux) {
+                            if(contadorSegundos == 20) {
+                                color = Color.Yellow;
+                                numero = 1;
+                                sleep = 500;
+                            } else {
+                                color = Color.Green;
+                            }
+                        } else {
+                            color = Color.White;
+                            numero--;
+                            contadorSegundos--;
+                        }
+
+                        banderaAux = !banderaAux;
+                    } else if(contadorSegundos < 24) { //Fijo | 03 segundos | Amarillo
+                        numero = contadorSegundos - 19; //Vuelve a ir del 1-3.
+                        color = Color.Yellow;
+                        sleep = 1000;
+
+                        if(contadorSegundos == 23) {
+                            numero = 3;
+                            color = Color.White;
+                            sleep = 500;
+                        }
+                    } else if(contadorSegundos < 27) { //Fijo | 02 segundos | Rojo
+                        numero = contadorSegundos - 23; //Vuelve a ir del 1-2.
+                        color = Color.Red;
+                        sleep = contadorSegundos == 24 ? 500 : 1000;
+
+                        if(contadorSegundos == 26) {
+                            numero = 2;
+                            color = Color.White;
+                            sleep = 500;
+                        }
+                    }
+                } else { //MODO PREVENTIVAS
+                    numero = 0;
+                    color = banderaPreventivas ? Color.Yellow : Color.White;
                     sleep = 1000;
-
-                    if(contadorSegundos == 23) {
-                        numero = 3;
-                        colorAux = Color.White;
-                        sleep = 500;
-                    }
-                } else if(contadorSegundos < 27) { //Fijo | 02 segundos | Rojo
-                    numero = contadorSegundos - 23; //Vuelve a ir del 1-2.
-                    colorAux = Color.Red;
-                    sleep = contadorSegundos == 24 ? 500 : 1000;
-
-                    if(contadorSegundos == 26) {
-                        numero = 2;
-                        colorAux = Color.White;
-                        sleep = 500;
-                    }
+                    banderaPreventivas = !banderaPreventivas;
+                    contadorSegundos = 0; //Que vuelva a iniciar siempre (cuando apaguen preventivas)
+                    semaforoVerticalEncendido = true; //Que vuelva a iniciar con el semaforo vertical
                 }
                 
-                color = colorAux;
                 Thread.Sleep(sleep);
                 Invoke(dActualizarVista);
                 contadorSegundos++;
 
-                timeMeasure.Stop();
-                Console.WriteLine($"Tiempo: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+                //timeMeasure.Stop();
+                //Console.WriteLine($"Tiempo: {timeMeasure.Elapsed.TotalMilliseconds} ms");
             }
             
             apagar();
@@ -147,9 +148,23 @@ namespace Semaforo {
             apagar();
         }
 
-        private void btnPausa_Click(object sender, EventArgs e) {
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            preventivasOn = chkPreventivas.Checked;
+        }
+
+        private void btnIniciar_Click_1(object sender, EventArgs e) {
+            iniciarSimulacion();
+        }
+
+        private void btnPausar_Click(object sender, EventArgs e) {
             apagar();
             pausarPrograma = true;
         }
+
+        private void btnDetener_Click(object sender, EventArgs e) {
+            detenerSimulacion();
+        }
     }
+
+
 }
