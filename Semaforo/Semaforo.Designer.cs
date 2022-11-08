0162fee8bc70a6882962bd0a71068522f3fcb49a
Alan Peña <alancin7@live.com.mx>
2022-11-08 12:38:50 -0600

    Disenio v1.5

--- Semaforo/Semaforo.Designer.cs
+++ Semaforo/Semaforo.Designer.cs
@@ -24,13 +24,14 @@ namespace Semaforo {
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Semaforo));
             this.lblnumero = new System.Windows.Forms.Label();
             this.lblSemaforo = new System.Windows.Forms.Label();
             this.chkPreventivas = new System.Windows.Forms.CheckBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnIniciar = new FontAwesome.Sharp.IconButton();
-            this.btnPausar = new FontAwesome.Sharp.IconButton();
             this.btnDetener = new FontAwesome.Sharp.IconButton();
+            this.btnPausar = new FontAwesome.Sharp.IconButton();
+            this.btnIniciar = new FontAwesome.Sharp.IconButton();
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
@@ -39,8 +40,8 @@ namespace Semaforo {
             // lblnumero
             // 
             this.lblnumero.AutoSize = true;
-            this.lblnumero.Font = new System.Drawing.Font("Digital-7 Mono", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblnumero.Location = new System.Drawing.Point(421, 61);
+            this.lblnumero.Font = new System.Drawing.Font("Digital-7 Mono", 89.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero.Location = new System.Drawing.Point(344, 269);
             this.lblnumero.Name = "lblnumero";
             this.lblnumero.Size = new System.Drawing.Size(107, 123);
             this.lblnumero.TabIndex = 3;
@@ -49,7 +50,7 @@ namespace Semaforo {
             // lblSemaforo
             // 
             this.lblSemaforo.AutoSize = true;
-            this.lblSemaforo.Location = new System.Drawing.Point(176, 274);
+            this.lblSemaforo.Location = new System.Drawing.Point(280, 317);
             this.lblSemaforo.Name = "lblSemaforo";
             this.lblSemaforo.Size = new System.Drawing.Size(35, 13);
             this.lblSemaforo.TabIndex = 6;
@@ -58,15 +59,15 @@ namespace Semaforo {
             // chkPreventivas
             // 
             this.chkPreventivas.AutoSize = true;
-            this.chkPreventivas.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.chkPreventivas.BackColor = System.Drawing.Color.Gold;
             this.chkPreventivas.Cursor = System.Windows.Forms.Cursors.Hand;
             this.chkPreventivas.Dock = System.Windows.Forms.DockStyle.Right;
             this.chkPreventivas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkPreventivas.Location = new System.Drawing.Point(781, 16);
+            this.chkPreventivas.Location = new System.Drawing.Point(649, 16);
             this.chkPreventivas.Name = "chkPreventivas";
-            this.chkPreventivas.Size = new System.Drawing.Size(125, 58);
+            this.chkPreventivas.Size = new System.Drawing.Size(161, 58);
             this.chkPreventivas.TabIndex = 8;
-            this.chkPreventivas.Text = "Preventivas";
+            this.chkPreventivas.Text = "PREVENTIVAS";
             this.chkPreventivas.UseVisualStyleBackColor = false;
             this.chkPreventivas.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
@@ -80,51 +81,9 @@ namespace Semaforo {
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(909, 77);
+            this.groupBox1.Size = new System.Drawing.Size(813, 77);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
-            // 
-            // btnIniciar
-            // 
-            this.btnIniciar.BackColor = System.Drawing.Color.LightGreen;
-            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIniciar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciar.IconChar = FontAwesome.Sharp.IconChar.Play;
-            this.btnIniciar.IconColor = System.Drawing.Color.Black;
-            this.btnIniciar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIniciar.Location = new System.Drawing.Point(3, 16);
-            this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Size = new System.Drawing.Size(147, 58);
-            this.btnIniciar.TabIndex = 10;
-            this.btnIniciar.Text = "Iniciar";
-            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIniciar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnIniciar.UseVisualStyleBackColor = false;
-            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click_1);
-            // 
-            // btnPausar
-            // 
-            this.btnPausar.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.btnPausar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPausar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnPausar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPausar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPausar.IconChar = FontAwesome.Sharp.IconChar.Pause;
-            this.btnPausar.IconColor = System.Drawing.Color.Black;
-            this.btnPausar.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnPausar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPausar.Location = new System.Drawing.Point(150, 16);
-            this.btnPausar.Name = "btnPausar";
-            this.btnPausar.Size = new System.Drawing.Size(147, 58);
-            this.btnPausar.TabIndex = 11;
-            this.btnPausar.Text = "Pausar";
-            this.btnPausar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnPausar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
-            this.btnPausar.UseVisualStyleBackColor = false;
-            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
             // 
             // btnDetener
             // 
@@ -147,14 +106,58 @@ namespace Semaforo {
             this.btnDetener.UseVisualStyleBackColor = false;
             this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
             // 
+            // btnPausar
+            // 
+            this.btnPausar.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnPausar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPausar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPausar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPausar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPausar.IconChar = FontAwesome.Sharp.IconChar.Pause;
+            this.btnPausar.IconColor = System.Drawing.Color.Black;
+            this.btnPausar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnPausar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPausar.Location = new System.Drawing.Point(150, 16);
+            this.btnPausar.Name = "btnPausar";
+            this.btnPausar.Size = new System.Drawing.Size(147, 58);
+            this.btnPausar.TabIndex = 11;
+            this.btnPausar.Text = "Pausar";
+            this.btnPausar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPausar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPausar.UseVisualStyleBackColor = false;
+            this.btnPausar.Click += new System.EventHandler(this.btnPausar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.LightGreen;
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnIniciar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.IconChar = FontAwesome.Sharp.IconChar.Play;
+            this.btnIniciar.IconColor = System.Drawing.Color.Black;
+            this.btnIniciar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnIniciar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.Location = new System.Drawing.Point(3, 16);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(147, 58);
+            this.btnIniciar.TabIndex = 10;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIniciar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click_1);
+            // 
             // panel1
             // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel1.Controls.Add(this.lblnumero);
             this.panel1.Controls.Add(this.lblSemaforo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 77);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(909, 583);
+            this.panel1.Size = new System.Drawing.Size(813, 746);
             this.panel1.TabIndex = 10;
             // 
             // Semaforo
@@ -162,10 +165,13 @@ namespace Semaforo {
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(909, 660);
+            this.ClientSize = new System.Drawing.Size(813, 823);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
-            this.MinimumSize = new System.Drawing.Size(800, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 39);
             this.Name = "Semaforo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Simulador semaforo";
